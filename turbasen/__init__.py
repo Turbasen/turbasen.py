0328ad011c3159a721b03f36067aa43cb89d7b84
Ali Kaafarani <ali@kvikshaug.no>
2015-06-09 15:06:07 +0200

    Bump patch version

--- turbasen/__init__.py
+++ turbasen/__init__.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 # Import the models we want directly available through the 'turbasen' module
 from .models import \
