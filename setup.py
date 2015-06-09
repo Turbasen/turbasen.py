acbf70ef7eb5f40d10343573cdafc22b3f6b5ec7
Ali Kaafarani <ali@kvikshaug.no>
2015-06-09 13:52:21 +0200

    Add OS Independent classifier

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: Norwegian',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
