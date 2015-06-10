1ce3862504c066c678b36aea31b0af331a4bcd8e
Ali Kaafarani <ali@kvikshaug.no>
2015-06-10 15:43:29 +0200

    Add 'api.get_object' event test

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,7 +1,9 @@
+import pytest
+
 from turbasen.events import trigger
 import turbasen
 
-def test_events():
+def test_custom_event():
     global mutable_list
     mutable_list = []
 
@@ -12,5 +14,18 @@ def test_events():
     turbasen.handle_event('foo', add_value_to_list)
     trigger('foo')
     trigger('bar')
+
+    assert len(mutable_list) == 1
+
+@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+def test_get_event():
+    global mutable_list
+    mutable_list = []
+
+    def add_value_to_list():
+        mutable_list.append(1)
+
+    turbasen.handle_event('api.get_object', add_value_to_list)
+    turbasen.Sted.get('52407fb375049e561500004e')
 
     assert len(mutable_list) == 1
