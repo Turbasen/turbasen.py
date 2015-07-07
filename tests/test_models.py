764319ddd52adc7e5269a7a516caa3ad9d55da2b
Ali Kaafarani <ali@kvikshaug.no>
2015-07-07 13:22:17 +0200

    Set up custom test objects

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,31 @@ import pytest
 
 import turbasen
 
+class ObjectManager:
+    def __init__(self):
+        self.sted = turbasen.Sted(
+            lisens='Privat',
+            status='Kladd',
+            navn='Testhytta',
+            beskrivelse='Testhytta er en opplevelse for seg selv',
+        )
+
+        self.gruppe = turbasen.Gruppe(
+            lisens='Privat',
+            status='Kladd',
+            navn='Testgruppe',
+        )
+
+        self.omrade = turbasen.Omrade(
+            lisens='Privat',
+            status='Kladd',
+            navn='Testområde',
+        )
+
+@pytest.fixture
+def object_manager():
+    return ObjectManager()
+
 @pytest.fixture
 def configure_dev():
     turbasen.configure(ENDPOINT_URL='http://dev.nasjonalturbase.no')
@@ -13,113 +38,111 @@ def configure_dev():
 def no_etag_cache():
     turbasen.configure(ETAG_CACHE_PERIOD=0)
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_get_sted(configure_dev):
-    sted = turbasen.Sted.get('52407fb375049e561500004e')
-    assert sted.navn == 'Tjørnbrotbu'
-    assert sted.ssr_id == 382116
+class TestClass:
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_get_gruppe(configure_dev):
-    gruppe = turbasen.Gruppe.get('52407f3c4ec4a13815000173')
-    assert gruppe.navn == 'Alta og omegn turlag'
+    #
+    # Fixtures to POST sample data before running tests
+    #
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_get_omrade(configure_dev):
-    omrade = turbasen.Omrade.get('52408144e7926dcf15000004')
-    assert omrade.navn == 'Sørlandet'
+    @pytest.fixture
+    def post_managed_sted(self, request, object_manager):
+        object_manager.sted.save()
+        request.addfinalizer(object_manager.sted.delete)
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_post(configure_dev):
-    sted = turbasen.Sted(
-        lisens='Privat',
-        status='Kladd',
-        navn='Testhytta',
-        geojson={
-            'properties': {'altitude': -999}, 'type': 'Point', 'coordinates': [12.18104163626816, 60.368712513406365]
-        },
-        fylke='Testfylket',
-        beskrivelse='Testhytta er en opplevelse for seg selv',
-        tags=[],
-        bilder=[],
-    )
-    assert sted.object_id is None
+    @pytest.fixture
+    def post_managed_gruppe(self, request, object_manager):
+        object_manager.gruppe.save()
+        request.addfinalizer(object_manager.gruppe.delete)
 
-    # POST the object and see that we've now defined its object_id
-    sted.save()
-    assert sted.object_id is not None
+    @pytest.fixture
+    def post_managed_omrade(self, request, object_manager):
+        object_manager.omrade.save()
+        request.addfinalizer(object_manager.omrade.delete)
 
-    # See that we can retrieve our POSTed data
-    assert sted.beskrivelse == turbasen.Sted.get(sted.object_id).beskrivelse
+    #
+    # Tests
+    #
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_put(configure_dev):
-    sted = turbasen.Sted.get('52407fb375049e561500004e')
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_get_sted(self, configure_dev, object_manager, post_managed_sted):
+        sted = turbasen.Sted.get(object_manager.sted.object_id)
+        assert sted.navn == object_manager.sted.navn
 
-    # Change some data
-    navn_original = sted.navn
-    navn_reversed = sted.navn[::-1]
-    assert navn_reversed != sted.navn
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_get_gruppe(self, configure_dev, object_manager, post_managed_gruppe):
+        gruppe = turbasen.Gruppe.get(object_manager.gruppe.object_id)
+        assert gruppe.navn == 'Testgruppe'
 
-    # PUT the object with the new data
-    sted.navn = navn_reversed
-    sted.save()
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_get_omrade(self, configure_dev, object_manager, post_managed_omrade):
+        omrade = turbasen.Omrade.get(object_manager.omrade.object_id)
+        assert omrade.navn == 'Testområde'
 
-    # See that we can retrieve our changed data
-    sted = turbasen.Sted.get('52407fb375049e561500004e')
-    assert navn_original != sted.navn
-    assert navn_reversed == sted.navn
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_post(self, configure_dev, object_manager):
+        sted = object_manager.sted
+        assert sted.object_id is None
 
-    # Reset the name, just in case
-    sted.navn = navn_original
-    sted.save()
+        # POST the object and see that we've now defined its object_id
+        sted.save()
+        assert sted.object_id is not None
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_delete(configure_dev):
-    sted = turbasen.Sted(
-        lisens='Privat',
-        status='Kladd',
-        navn='Testhytta',
-        geojson={
-            'properties': {'altitude': -999}, 'type': 'Point', 'coordinates': [12.18104163626816, 60.368712513406365]
-        },
-        fylke='Testfylket',
-        beskrivelse='Testhytta er en opplevelse for seg selv',
-        tags=[],
-        bilder=[],
-    )
-    assert sted.object_id is None
+        # See that we can retrieve our POSTed data
+        assert sted.beskrivelse == turbasen.Sted.get(sted.object_id).beskrivelse
 
-    # POST the object and see that we've now defined its object_id
-    sted.save()
-    object_id = sted.object_id
-    assert sted.object_id is not None
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_put(self, configure_dev, object_manager, post_managed_sted):
+        sted = object_manager.sted
 
-    # See that we can delete it
-    sted.delete()
-    assert sted.object_id is None
-    with pytest.raises(turbasen.exceptions.DocumentNotFound):
-        turbasen.Sted.get(object_id)
+        # Change some data
+        navn_original = sted.navn
+        navn_reversed = sted.navn[::-1]
+        assert navn_reversed != sted.navn
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_refresh(configure_dev, no_etag_cache):
-    sted = turbasen.Sted.get('52407fb375049e561500004e')
-    etag = sted._etag
-    sted._refresh()
-    assert etag == sted._etag
+        # PUT the object with the new data
+        sted.navn = navn_reversed
+        sted.save()
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_lookup(configure_dev):
-    results = turbasen.Sted.lookup(pages=2)
-    result_list = list(results)
-    assert len(result_list) == turbasen.settings.Settings.LIMIT * 2
-    assert result_list[0].object_id != ''
+        # See that we can retrieve our changed data
+        sted = turbasen.Sted.get(sted.object_id)
+        assert navn_original != sted.navn
+        assert navn_reversed == sted.navn
 
-@pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
-def test_extra(configure_dev):
-    sted = turbasen.Sted(navn='Heia', foo_bar=42)
-    assert hasattr(sted, 'navn')
-    assert not hasattr(sted, 'foo_bar')
-    assert 'foo_bar' in sted._extra
-    assert 'foo_bar' in sted.get_data(include_extra=True)
-    assert 'foo_bar' not in sted.get_data(include_extra=False)
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_delete(self, configure_dev, object_manager):
+        sted = object_manager.sted
+        assert sted.object_id is None
+
+        # POST the object and see that we've now defined its object_id
+        sted.save()
+        object_id = sted.object_id
+        assert sted.object_id is not None
+
+        # See that we can delete it
+        sted.delete()
+        assert sted.object_id is None
+        with pytest.raises(turbasen.exceptions.DocumentNotFound):
+            turbasen.Sted.get(object_id)
+
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_refresh(self, configure_dev, object_manager, post_managed_sted):
+        sted = object_manager.sted
+        etag = sted._etag
+        sted._refresh()
+        assert etag == sted._etag
+
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_lookup(self, configure_dev):
+        results = turbasen.Sted.lookup(pages=2)
+        result_list = list(results)
+        assert len(result_list) == turbasen.settings.Settings.LIMIT * 2
+        assert result_list[0].object_id != ''
+
+    @pytest.mark.skipif(turbasen.settings.Settings.API_KEY is None, reason="API key not set")
+    def test_extra(self, configure_dev):
+        sted = turbasen.Sted(navn='Heia', foo_bar=42)
+        assert hasattr(sted, 'navn')
+        assert not hasattr(sted, 'foo_bar')
+        assert 'foo_bar' in sted._extra
+        assert 'foo_bar' in sted.get_data(include_extra=True)
+        assert 'foo_bar' not in sted.get_data(include_extra=False)
