7bf8e7a68f5230be12af6b64cb3659a750b1c867
Ali Kaafarani <ali@kvikshaug.no>
2016-05-15 16:01:23 +0200

    Remove future imports

--- turbasen/__init__.py
+++ turbasen/__init__.py
@@ -1,6 +1,3 @@
-# encoding: utf-8
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 # Import the models we want directly available through the root module
 from .models import (
     Bilde,
--- turbasen/exceptions.py
+++ turbasen/exceptions.py
@@ -1,6 +1,3 @@
-# encoding: utf-8
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 class DocumentNotFound(Exception):
     """Thrown when a request for a document with a given object id isn't found in Turbasen"""
     pass
