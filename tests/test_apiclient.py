25305f387aab029206f63a07dda3ab80bd330659
Ali Kaafarani <ali@kvikshaug.no>
2017-05-12 13:39:57 +0200

    test: list query with no documents

--- tests/test_apiclient.py
+++ tests/test_apiclient.py
@@ -135,6 +135,11 @@ class TestClass(unittest.TestCase):
         self.assertEqual(len(result_list), turbasen.settings.Settings.LIMIT * 2)
         self.assertNotEqual(result_list[0]['_id'], '')
 
+    def test_list_empty(self):
+        results = turbasen.Sted.list(params={'foo': 404})
+        result_list = list(results)
+        self.assertEqual(len(result_list), 0)
+
     def test_list_fields(self):
         results = turbasen.Sted.list(pages=1, params={
             'fields': ['betjeningsgrad'],
